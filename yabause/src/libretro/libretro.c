ae1ba7b81ab93365fc9b509616b109b1a5e90131
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-03-24 16:58:02 -0400

    Update libretro.c

--- yabause/src/libretro/libretro.c
+++ yabause/src/libretro/libretro.c
@@ -338,8 +338,8 @@ static int PERLIBRETROHandleEvents(void)
                // (input_state_cb_wrapper(i, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START) ? PerKeyDown((i << 8) + PERMOUSE_START) : PerKeyUp((i << 8) + PERMOUSE_START));
                // is saturn supposed to be able to use several mouse ?
                // because i don't think this code is right in that case
-               s32 dispx = input_state_cb_wrapper(i, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_X);
-               s32 dispy = input_state_cb_wrapper(i, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_Y);
+               s32 dispx = input_state_cb_wrapper(i, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_X);
+               s32 dispy = input_state_cb_wrapper(i, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_Y);
                PerMouseMove(mousebits, dispx, -dispy);
                break;
            
