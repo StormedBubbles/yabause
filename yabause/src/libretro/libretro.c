c746d4386fad52c6d09ab56d947c2b6ef6263714
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-05-05 12:22:44 -0400

    Update libretro.c

--- yabause/src/libretro/libretro.c
+++ yabause/src/libretro/libretro.c
@@ -341,7 +341,7 @@ static int PERLIBRETROHandleEvents(void)
                s32 dispx = input_state_cb_wrapper(i, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_X);
                s32 dispy = input_state_cb_wrapper(i, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_Y);
                //PerMouseMove(mousebits, dispx, -dispy);
-               PerMouseMove(mousebits, 0, 0);
+               PerMouseMove(mousebits, 100, 0);
                break;
            
              case RETRO_DEVICE_LIGHTGUN:
